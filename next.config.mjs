137347d3e29e16511dd13a318a53cd028fb1db88
Khosbayar707 <torutora0415@gmail.com>
2025-04-12 20:56:02 +0800

    4

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  transpilePackages: ["@clerk/nextjs"], // Helps Clerk work with App Router + Edge Middleware
-  experimental: {
-    runtime: "edge", // Ensures Edge-compatible modules
-  },
-  images: {
-    domains: ["images.clerk.dev", "lh3.googleusercontent.com"], // Optional: Clerk avatar/image support
-  },
 };
 
 export default nextConfig;
